1fd0d968e9b85ff7f95f20d43cd5257cda4b71f2
qv1ntv5 <sanmillan.german@gmail.com>
2024-08-07 20:02:38 +0200

    Heuristic

--- AVEvasion/AVEvasionWithC/Heuristic Scan/Delay.cs
+++ AVEvasion/AVEvasionWithC/Heuristic Scan/Delay.cs
@@ -1,6 +1,6 @@
 [DllImport("kernel32.dll")]
-static extern void Sleep(uint dwMilliseconds);
-        
+static extern void Sleep(uint dwMilliseconds); //Invoca la Win32 API 'Sleep' a través de la librería Kernel32.
+
 static void Main(string[] args)
 {
     DateTime t1 = DateTime.Now;
@@ -10,3 +10,5 @@ static void Main(string[] args)
     {
         return;
     }
+}//Guarda la variación de tiempo antes y después de llamar a Sleep() durante 2000 milisegundos. Seguidamente, si el tiempo transcurrido es menor de 1,5 segundos se da por hecho que no se ha ejecutado el Sleep y se sale del código.
+
